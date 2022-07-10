fd9b54de605ebf21f64b4cdbe8ef531448c13d80
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-07-10 21:12:02 +1000

    guild list

---
+++ src/modules/commands/guilds.js
@@ -0,0 +1,16 @@
+const client = require("./client");
+
+module.exports = {
+    config: {
+        name: "guilds",
+        description: "Lists the guilds the bot is in.",
+    },
+    exec: async (message) => {
+        const guilds = client.guilds.cache.map((g) => g.name).join("\n");
+
+        message.channel.send(`
+**Guilds**
+
+${guilds}`);
+    },
+};
--- src/modules/commands/index.js
+++ src/modules/commands/index.js
@@ -3,6 +3,7 @@ const MENTION_RESPONSES = require("../responses/mentions");
 
 module.exports = {
     ping: require("./ping"),
+    guilds: require("./guilds"),
 };
 
 client.on("messageCreate", async (message) => {
@@ -13,5 +14,14 @@ client.on("messageCreate", async (message) => {
     } else if (message.content.trim().includes(`<@${process.env.CLIENT_ID}>`)) {
         const response = MENTION_RESPONSES[Math.floor(Math.random() * MENTION_RESPONSES.length)];
         message.reply(response);
+    }
+
+    const args = message.content.split(" ");
+    const command = `${args.shift().replace(process.env.PREFIX, "").toLowerCase()}`;
+
+    if (!message.content.startsWith(process.env.PREFIX)) return;
+
+    if (module.exports[command]) {
+        module.exports[command].exec(message);
     }
 });
